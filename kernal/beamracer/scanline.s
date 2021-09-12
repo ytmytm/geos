ae46a6266768048465ef3d7adba8b0632a80a5d2
Maciej Witkowiak <mwitkowiak@gmail.com>
2021-09-13 00:00:12 +0200

    remove GetScanLine tables for VIC screen, release some space in KERNAL and DLGBOXRAMBUF segments

--- kernal/beamracer/scanline.s
+++ kernal/beamracer/scanline.s
@@ -11,9 +11,12 @@
 .include "kernal.inc"
 .include "c64.inc"
 
-.global _GetScanLine_BR
+.import Panic
 
-.segment "dlgboxrambuf"
+.global _GetScanLine_BR
+.global _GetScanLine
+
+.segment "graph2n"
 ;---------------------------------------------------------------
 ; GetScanLine                                             $C13C
 ;
@@ -24,6 +27,13 @@
 ;            r6  add of 1st byte of background scr
 ; Destroyed: a
 ;---------------------------------------------------------------
+_GetScanLine:
+	; dummy, point to foreground in case Application would draw something directly
+	LoadW r5, SCREEN_BASE
+	LoadW r6, SCREEN_BASE
+	rts
+
+	; this is used by all Kernal functions
 _GetScanLine_BR:
 	txa
 	pha
@@ -65,49 +75,4 @@ _GetScanLine_BR:
 
 @5:	;AddVW br_screen_base+$0F00, r5	; !ST_WR_FORE && !ST_WR_BACK ?!
 	;bra @1
-.import Panic
 	jmp Panic
-
-.global _GetScanLine
-
-.segment "graph2n"
-
-;---------------------------------------------------------------
-; GetScanLine                                             $C13C
-;
-; Function:  Returns the address of the beginning of a scanline
-
-; Pass:      x   scanline nbr
-; Return:    r5  add of 1st byte of foreground scr
-;            r6  add of 1st byte of background scr
-; Destroyed: a
-;---------------------------------------------------------------
-_GetScanLine:
-	txa
-	pha
-	pha
-	and #%00000111
-	sta r6H
-	pla
-	lsr
-	lsr
-	lsr
-	tax
-	lda LineTabL,x
-	ora r6H
-	sta r5L
-	sta r6L
-	lda LineTabH,x
-	sta r5H
-	sta r6H
-	pla
-	tax
-	rts
-
-.segment "graph2o"
-
-.define LineTab SCREEN_BASE+0*320, SCREEN_BASE+1*320, SCREEN_BASE+2*320, SCREEN_BASE+3*320, SCREEN_BASE+4*320, SCREEN_BASE+5*320, SCREEN_BASE+6*320, SCREEN_BASE+7*320, SCREEN_BASE+8*320, SCREEN_BASE+9*320, SCREEN_BASE+10*320, SCREEN_BASE+11*320, SCREEN_BASE+12*320, SCREEN_BASE+13*320, SCREEN_BASE+14*320, SCREEN_BASE+15*320, SCREEN_BASE+16*320, SCREEN_BASE+17*320, SCREEN_BASE+18*320, SCREEN_BASE+19*320, SCREEN_BASE+20*320, SCREEN_BASE+21*320, SCREEN_BASE+22*320, SCREEN_BASE+23*320, SCREEN_BASE+24*320
-LineTabL:
-	.lobytes LineTab
-LineTabH:
-	.hibytes LineTab
